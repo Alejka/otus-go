76cfe6b9dface0f67b4bd9031974bfe01f171d91
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2019-10-09 21:57:01 +0300

    refactor

--- hw-1/main.go
+++ hw-1/main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"time"
 
 	"github.com/beevik/ntp"
 )
 
-type clock struct {
-	year  int
-	month int
-	day   int
-}
-
 func main() {
-	currentcClock := new(clock)
-	ntpClock := new(clock)
-
 	currTime := time.Now()
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
-
-	if err == nil {
-		currentcClock.year, currentcClock.month, currentcClock.day = currTime.Clock()
-		ntpClock.year, ntpClock.month, ntpClock.day = ntpTime.Clock()
-
-		fmt.Printf("Current time: %v:%v:%v | Exact time (NTP): %v:%v:%v", currentcClock.year, currentcClock.month, currentcClock.day, ntpClock.year, ntpClock.month, ntpClock.day)
-	} else {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	if err != nil {
+		log.Fatal("Error:\n", err)
 	}
+
+	fmt.Printf("Current time: %v | Exact time (NTP): %v\n", currTime.Format("15:04:05"), ntpTime.Format("15:04:05"))
 }
