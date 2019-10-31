f20cd42b1266d405c9198ac9e6d0743353aaf5bb
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2019-10-31 21:31:18 +0200

    2 version

--- hw-2/unpack.go
+++ hw-2/unpack.go
@@ -1,28 +1,14 @@
 package hw2
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
-
-	//"unicode"
 	"unicode/utf8"
 
 	valid "github.com/asaskevich/govalidator"
 )
 
 func Unpack(str string) string {
-	// "a4bc2d5e" => "aaaabccddddde"
-	// "abcd" => "abcd"
-	// "45" => "" (некорректная строка)
-
-	// qwe\4\5 => qwe45 (*)
-	// qwe\45 => qwe44444 (*)
-	// qwe\\5 => qwe\\\\\ (*)
-
-	// qwe\5 => qwe5 (*)
-	// qwe\\5 => qwe\\\\\ (*)
-
 	runesCount := utf8.RuneCountInString(str)
 
 	if !utf8.ValidString(str) || runesCount == 0 {
@@ -30,28 +16,21 @@ func Unpack(str string) string {
 	}
 
 	var unpackedStrBuilder strings.Builder
+
 	var repeatCountStrBuilder strings.Builder
 	repeatRune := rune(-1)
 	repeatCount := 1
-	runeIndex := 0
-	isEscapedLastRune := false
 
-	// a4bc2d5e => aaaabccddddde
-	fmt.Printf("-----------\nNEXT STR: %q\nCount of runes: %d\n-----------\n", str, runesCount)
+	isEscapeLastRune := false
+
 	for _, rune := range str {
-		runeIndex++
-		fmt.Printf("runeIndex: %d\nrune: %q\n", runeIndex, rune)
-
-		// https://stackoverflow.com/questions/25540951/differences-between-isdigit-and-isnumber-in-unicode-in-go
-		// http://www.fileformat.info/info/unicode/category/Nd/list.htm
-		//if unicode.IsDigit(rune) {
-		if valid.IsInt(string(rune)) && !isEscapedLastRune { // if rune is digit
+		if valid.IsInt(string(rune)) && !isEscapeLastRune { // if rune is digit (not escape)
 			repeatCountStrBuilder.WriteRune(rune)
 			continue
 		} else {
 			// if rune is "escape" character
-			if rune == '\\' && !isEscapedLastRune {
-				isEscapedLastRune = true
+			if rune == '\\' && !isEscapeLastRune {
+				isEscapeLastRune = true
 			} else {
 				// if rune is char: repeat previous rune
 				if repeatRune != -1 {
@@ -65,37 +44,20 @@ func Unpack(str string) string {
 
 				repeatRune = rune
 				repeatCountStrBuilder.Reset()
-				isEscapedLastRune = false
+				isEscapeLastRune = false
 			}
 		}
-
-		/*
-		   if runeIndex == runesCount {
-		       if repeatRune != -1 {
-		           if repeatCountStrBuilder.Len() > 0 {
-		               repeatCount, _ = strconv.Atoi(repeatCountStrBuilder.String())
-		           } else {
-		               repeatCount = 1
-		           }
-		           unpackedStrBuilder.WriteString(strings.Repeat(string(repeatRune), repeatCount))
-		       }
-		   }
-		*/
 	}
 
-	fmt.Printf("process last rune: \n")
+	// process last rune
 	if repeatRune != -1 {
 		if repeatCountStrBuilder.Len() > 0 {
 			repeatCount, _ = strconv.Atoi(repeatCountStrBuilder.String())
 		} else {
 			repeatCount = 1
 		}
-		fmt.Printf("repeatCount: %d\nrepeatRune: %q\n", repeatCount, repeatRune)
 		unpackedStrBuilder.WriteString(strings.Repeat(string(repeatRune), repeatCount))
 	}
 
-	tmpstr := unpackedStrBuilder.String()
-	fmt.Printf("READY STR: %q\n", tmpstr)
-	return tmpstr
-	//return unpackedStrBuilder.String()
+	return unpackedStrBuilder.String()
 }
--- hw-2/unpack_test.go
+++ hw-2/unpack_test.go
@@ -2,41 +2,11 @@ package hw2
 
 import (
 	"testing"
-	//"strconv"
 
 	"github.com/stretchr/testify/require"
 )
 
-/*func TestRepeatRunes(t *testing.T) {
-    var r rune
-    var repeatCount int
-
-    r = 'a'
-    repeatCount = 0
-    require.Equal(t, "", repeatRunes(r, repeatCount), "Repeating a rune '"+string(r)+"', "+strconv.Itoa(repeatCount)+" times")
-
-    r = 'o'
-    repeatCount = -7
-    require.Equal(t, "", repeatRunes(r, repeatCount), "Repeating a rune '"+string(r)+"', "+strconv.Itoa(repeatCount)+" times")
-
-    r = 'z'
-    repeatCount = 1
-    require.Equal(t, "z", repeatRunes(r, repeatCount), "Repeating a rune '"+string(r)+"', "+strconv.Itoa(repeatCount)+" times")
-
-    r = 'q'
-    repeatCount = 5
-    require.Equal(t, "qqqqq", repeatRunes(r, repeatCount), "Repeating a rune '"+string(r)+"', "+strconv.Itoa(repeatCount)+" times")
-}*/
-
 func TestUnpack(t *testing.T) {
-	// "a4bc2d5e" => "aaaabccddddde"
-	// "abcd" => "abcd"
-	// "45" => "" (некорректная строка)
-
-	// qwe\4\5 => qwe45 (*)
-	// qwe\45 => qwe44444 (*)
-	// qwe\\5 => qwe\\\\\ (*)
-
 	var str string
 
 	str = `qwe\4\5`
@@ -62,6 +32,12 @@ func TestUnpack(t *testing.T) {
 
 	str = "abcd"
 	require.Equal(t, "abcd", Unpack(str), "Unpacking a string: "+str)
+
+	str = "5"
+	require.Equal(t, "", Unpack(str), "Unpacking a string: "+str)
+
+	str = "a"
+	require.Equal(t, "a", Unpack(str), "Unpacking a string: "+str)
 
 	str = ""
 	require.Equal(t, "", Unpack(str), "Unpacking a string: "+str)
