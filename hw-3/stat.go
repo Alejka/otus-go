9fdf215e1b47c14dc178610a9188f308059417ae
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2019-11-14 21:36:25 +0200

    hw3 finish

--- hw-3/stat.go
+++ hw-3/stat.go
@@ -1,50 +1,50 @@
 package hw3
 
 import (
-	//"fmt"
+	"sort"
 	"strings"
 	"unicode"
-    "sort"
 )
 
 // Top10 returns the 10 most frequently occurring words
 func Top10(text string) []string {
-    text = strings.ToLower(text)
-    
-    f := func(c rune) bool {
-		//return !unicode.IsLetter(c)
-        return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	text = strings.ToLower(text)
+
+	// get words
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
 	}
-    words := strings.FieldsFunc(text, f) //strings.FieldsFunc(s, unicode.IsSpace)
-    
-    counts := make(map[string]int, len(words))
-    for _, w := range words {
-        counts[w]++
-    }
-    
-    type WordFreq struct {
-        word string
-        freq int
-    }
-    wordFreqList := make([]WordFreq, 0, len(counts))
-    for k, v := range counts {
-        wordFreqList = append(wordFreqList, WordFreq{k, v})
-    }
-    
-    sort.Slice(wordFreqList, func(i, j int) bool {
+	words := strings.FieldsFunc(text, f) //strings.FieldsFunc(s, unicode.IsSpace)
+
+	// get the count of each word
+	counts := make(map[string]int, len(words))
+	for _, w := range words {
+		counts[w]++
+	}
+
+	// sorting by count
+	type WordFreq struct {
+		word string
+		freq int
+	}
+	wordFreqList := make([]WordFreq, 0, len(counts))
+	for k, v := range counts {
+		wordFreqList = append(wordFreqList, WordFreq{k, v})
+	}
+	sort.Slice(wordFreqList, func(i, j int) bool {
 		return wordFreqList[i].freq > wordFreqList[j].freq
 	})
-    
-    count := len(wordFreqList)
-    if count > 10 {
-        count = 10
-    }
-    topWordFreqList := wordFreqList[:count]
-    
-    top := make([]string, 0, count)
-    for _, v := range topWordFreqList {
-        top = append(top, v.word)
-    }
-    
-    return top
+
+	// get top 10 words
+	count := len(wordFreqList)
+	if count > 10 {
+		count = 10
+	}
+	topWordFreqList := wordFreqList[:count]
+	top := make([]string, 0, count)
+	for _, v := range topWordFreqList {
+		top = append(top, v.word)
+	}
+
+	return top
 }
--- hw-3/stat_test.go
+++ hw-3/stat_test.go
@@ -7,20 +7,39 @@ import (
 )
 
 func TestTop10(t *testing.T) {
-    var text string
-    var top []string
-    var expected []string
-    var actual []string
-    
-    text = "slovo1 slovo2 slovo3 slovo1 slovo2 slovo1 slovo2 slovo1 fkdsfj fds dfksv slovo3 dljflskd sLovo1 sLovo1 ddsa sloVO1"
-    expected = []string{"slovo1", "slovo2", "slovo3"}
-    top = Top10(text)
-    actual = top[:3]
-    require.Equal(t, expected, actual, "Analyze a string: "+text)
-    
-    text = "slovo1 slovo2 slovo3 slovo1 slovo2 slovo1 slovo2 slovo1 fkdsfj fds dfksv slovo3 dljflskd sLovo1 sLovo1 ddsa sloVO1 645ппавп павпва п вапвапа  пвап вп"
-    expected = []string{"slovo1", "slovo2", "slovo3"}
-    top = Top10(text)
-    actual = top[:3]
-    require.Equal(t, expected, actual, "Analyze a string: "+text)
+	text := ""
+	top := make([]string, 0, 10)
+	expected := make([]string, 0, 10)
+	actual := make([]string, 0, 10)
+	message := ""
+
+	text = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla magna orci, auctor ut lacus sed, venenatis commodo neque. Integer justo mi, euismod nec euismod sit amet, ultricies sed nisi. Ut quis risus risus. Donec at ultricies arcu, nec blandit ligula. Aenean ut diam ut quam euismod dignissim cursus eget nibh. Mauris porttitor efficitur maximus. Morbi eget urna eu mi varius pharetra.
+Nunc efficitur arcu id dapibus ultricies. Morbi nisi elit, eleifend ut ipsum eget, commodo consequat magna. Pellentesque fringilla eros et mauris imperdiet, ut efficitur sapien cursus. Duis fringilla tellus eu purus feugiat scelerisque. Phasellus laoreet urna a odio viverra, at pretium sapien malesuada. Sed scelerisque eget sapien non luctus. Integer eu luctus est. Proin ut lectus aliquet, facilisis nunc ut, accumsan eros.
+Suspendisse viverra eu nibh vitae porttitor. Pellentesque molestie tempus leo, condimentum blandit magna molestie eu. Pellentesque suscipit vehicula fermentum. In laoreet lacus nulla, in volutpat elit ultrices mollis. Sed nisl tellus, lacinia vitae lacus vehicula, sodales convallis lorem. Curabitur accumsan tincidunt justo at mattis. Proin felis nisi, volutpat non ipsum vitae, fringilla euismod leo. Nullam tellus est, gravida a bibendum sit amet, viverra id nunc.
+Aliquam ut imperdiet quam. Donec congue consectetur mattis. Aenean elementum elit est, egestas tincidunt erat tempor et. Praesent mollis, odio ut dignissim scelerisque, nibh sapien sagittis eros, sed rhoncus odio justo ac orci. Curabitur fringilla vitae mi ac eleifend. Etiam consequat malesuada facilisis. Aliquam quis nisi hendrerit, efficitur libero vel, dapibus nisl. Maecenas tristique ex magna, sed sagittis ante cursus a.
+Proin non auctor mi, vel gravida nisl. Nam vehicula nibh eget suscipit porttitor. Vivamus velit nibh, lobortis egestas consectetur ut, accumsan at arcu. Donec imperdiet nibh in tincidunt dictum. Sed consequat ipsum quis velit cursus iaculis. Sed pulvinar dui quis purus dignissim semper. Maecenas dui nunc, sollicitudin vitae ultrices ac, malesuada vitae dolor. Fusce sit amet condimentum lorem. Maecenas ex neque, rutrum eget hendrerit facilisis, pulvinar in metus. Pellentesque eget quam vel tortor hendrerit porta cursus sed mauris.
+`
+	top = Top10(text)
+	expected = []string{"ut", "sed", "eget", "nibh", "vitae", "cursus", "eu"}
+	actual = top[:len(expected)]
+	message = "Analyze a string: " + text
+	require.Len(t, top, 10, message)
+	require.ElementsMatch(t, expected, actual, message)
+
+	text = "we have more than ten different words in this long test text"
+	top = Top10(text)
+	expected = []string{}
+	actual = top[:len(expected)]
+	message = "Analyze a string: " + text
+	require.Len(t, top, 10, message)
+	require.ElementsMatch(t, expected, actual, message)
+
+	text = "we have one repeat in this text: word word"
+	top = Top10(text)
+	expected = []string{"word"}
+	actual = top[:len(expected)]
+	message = "Analyze a string: " + text
+	require.Len(t, top, 8, message)
+	require.ElementsMatch(t, expected, actual, message)
 }
